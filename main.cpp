3fc4564e18f4a2a6d11c2848433d09c87181ea7a
sarugap <paw11297@gmail.com>
2016-10-20 22:23:06 +0200

    Shape detection3 - code optimization

--- main.cpp
+++ main.cpp
@@ -51,8 +51,6 @@ int main(int argc, char** argv)
 	medianBlur(gHSVImage[1],gHSVImage[1],(MedFilter,MedFilter));
 	merge(gHSVImage, 3, hHSVImageBase);
 	
-//	while(1)
-//	{
 		//Threshold the HSV image - BLUE COLOR
 		inRange(hHSVImageBase, Scalar(BlueLowH, BlueLowS, BlueLowV), Scalar(BlueHighH, BlueHighS, BlueHighV), gThresholdImage[0]); 
 
@@ -97,40 +95,35 @@ int main(int argc, char** argv)
 		vector<Point2f>center( contours.size() );
 		vector<float>radius( contours.size() );
 
-		for( int i = 0; i < contours.size(); i++ )
-			{ approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-			boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-			minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+		for(int i = 0; i < contours.size(); i++)
+			{ 
+				approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+				boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+				minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
 			}
 
 		float stosunekHW = 0.0;
 
 		/// Draw polygonal contour + bonding rects + circles
-		Mat drawing = Mat::zeros( gThresholdImage[0].size(), CV_8UC3 );
+		Mat drawing = Mat::zeros(gThresholdImage[0].size(), CV_8UC3);
 		for( int i = 0; i< contours.size(); i++ )
+		{
+			stosunekHW = boundRect[i].width/boundRect[i].height;
+			if((boundRect[i].width > 20)&&(boundRect[i].height > 20)&&(boundRect[i].width < 100)&&(boundRect[i].height < 100)&&(0.95 <= stosunekHW)&&(stosunekHW <= 1.05))
 			{
-				stosunekHW = boundRect[i].width/boundRect[i].height;
-				if((boundRect[i].width > 20)&&(boundRect[i].height > 20)&&(boundRect[i].width < 50)&&(boundRect[i].height < 50)&&(0.95 <= stosunekHW)&&(stosunekHW <= 1.05))
-				//if((0.9 <= stosunekHW)&&(stosunekHW <= 1.0))
-				{
-					Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-					drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-					rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-					circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-				}
+				Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+				drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+				rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+				//circle(drawing, center[i], (int)radius[i], color, 2, 8, 0);
 			}
+		}
 
-		/// Show in a window
 		namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
 		imshow( "Contours", drawing );
 
-
-		//imshow("Thresholded image - BLUE SIGNS - Edges", gThresholdImage[0]); 
-
-//		if(waitKey(10) == 27) break;
-//	}
 		waitKey(10);
 		getchar();
+
     return 0;
 }
 
