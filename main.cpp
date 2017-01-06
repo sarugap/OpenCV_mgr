42ff83b9a0acae413025295484982aecc0f9d1ff
sarugap <paw11297@gmail.com>
2017-01-06 21:29:15 +0100

    MSER dla wszystkich znaków.
    1. Gotowy program do testowania MSER dla wszystkich znaków drogowych.
    2. Optymalizacja programu.
    3. Usunięcie zbędnych fragmentów kodu.

--- main.cpp
+++ main.cpp
@@ -1,284 +1,75 @@
 #include "main.h"
 #include "matlab_fcn.h"
+#include "gui.h"
 #include <algorithm>
 #include <Windows.h>
 
-#define SIMPLE_IMG			0
+#define SIMPLE_IMG					0
 
-#define MSER_RED			1
-#define MSER_BLUE			0
-#define MSER_YELLOW			0
-#define MSER_WHITE			0
-#define MSER_BLACK			0
-
-unsigned int MSER_DELTA_RED			= 12;
-unsigned int MSER_MIN_AREA_RED		= 1000;
-unsigned int MSER_MAX_AREA_RED		= 15000;
-float MSER_VARIATION_RED			= 0.3;
-
-unsigned int MSER_DELTA_BLUE		= 7;
-unsigned int MSER_MIN_AREA_BLUE		= 1000;
-unsigned int MSER_MAX_AREA_BLUE		= 15000;
-float MSER_VARIATION_BLUE			= 0.3;
-
-RNG rng(12345);
-char file_path[256], tempString[100], mser_param[50];
 fstream fOriginalImg, fB_channel, fG_channel, fR_channel;
 
-//Praca na pojedynczym obrazie
-#if SIMPLE_IMG
+unsigned int flMserColor			= 0;
 
-int main(int argc, char** argv)
-{
-	cout << "--------------------------START--------------------------" << endl;
-	fB_channel.open("fB_channel.txt", std::ios::out);
-	fG_channel.open("fG_channel.txt", std::ios::out);
-	fR_channel.open("fR_channel.txt", std::ios::out);
-	fOriginalImg.open("gOriginal.txt", std::ios::out);
+_tMSER MSERred, MSERblue, MSERyellow, MSERwhite, MSERblack;
 
-	gOrginalImage = imread(argv[1]);
+char file_path[256], tempString[100], mser_param[50];
+char file_out_name[10], file_name[10], file_path_in[256], file_path_out[256], string_temp[256];
 
-	if(gOrginalImage.empty())
-	{
-		cout <<"Invalid path to image!" <<endl;
-		getchar();
-		return -1;
-	}
-
-	//Przycinanie obrazu
-	//ImCropp(gOrginalImage, gOrginalImage, Top_X, Top_Y, Bottom_X, Bottom_Y);
-
-	//Konwersja obrazu
-	gOrginalImage.convertTo(gOrginalImage, CV_32FC3); 
-	gOrginalImage = gOrginalImage/255;
-
-	//Rozdzielenie kanalow B, G, R
-	split(gOrginalImage, gBGRImage);
-	
-	ImAdjust(gBGRImage[2], gR_ch_main, 0.3, 1, 0, 1);
-	ImAdjust(gBGRImage[1], gG_ch_main, 0.5, 1, 0, 1);
-	ImAdjust(gBGRImage[0], gB_ch_main, 0.3, 1, 0, 1);
-	
-
-	//CZERWONE ZNAKI	[img_red = r_h.*(1-g).*(1-b)]
-	gR_channel = gR_ch_main.mul(1-gBGRImage[1]);
-	gR_channel = gR_channel.mul(1-gBGRImage[0]);
-
-	//NIEBIESKIE ZNAKI	[img_blue = b_h.*(1-r).*(1-g)]
-	gB_channel = gB_ch_main.mul(1-gBGRImage[2]);
-	gB_channel = gB_channel.mul(1-gBGRImage[1]);
-
-	//ZOLTE ZNAKI		[img_yellow = r_h.*g.*(1-b)]
-	gY_channel = gR_ch_main.mul(gBGRImage[1]);
-	gY_channel = gY_channel.mul(1-gBGRImage[0]);
-
-	//BIALE ZNAKI		[img_white = r_h.*b_h.*g_h]
-	gW_channel = gR_ch_main.mul(gB_ch_main);
-	gW_channel = gW_channel.mul(gG_ch_main);
-
-	//CZARNE ZNAKI		[img_black = (1-r).*(1-g).*(1-b)]
-	gK_channel = 1 - gBGRImage[2];
-	gK_channel = gK_channel.mul(1 - gBGRImage[1]);
-	gK_channel = gK_channel.mul(1 - gBGRImage[0]);
-
-	cout << "--------------------------MSER---------------------------" << endl;
-	
-		//Konwersja obrazow
-	gR_channel.convertTo(gR_channel, CV_8UC1, 255);
-	gY_channel.convertTo(gY_channel, CV_8UC1, 255);
-	gB_channel.convertTo(gB_channel, CV_8UC1, 255);
-	gW_channel.convertTo(gW_channel, CV_8UC1, 255);
-	gK_channel.convertTo(gK_channel, CV_8UC1, 255);
-
-//	line( gR_channel, Point(0, 0), Point(100, 100), Scalar( 255, 0, 0 ), 2, 8 );
-//	circle( gR_channel, Point(100, 100), 1, Scalar( 255, 0, 0 ), -1, 8);
-	
-//	MSER
-#if 0
-	{
-		MSER ms_red(MSER_DELTA_RED, MSER_MIN_AREA_RED, MSER_MAX_AREA_RED, MSER_VARIATION_RED);
-
-		vector<vector<Point>> regions_red;
-		vector<Point> temp_vect;
-		ms_red(gR_channel, regions_red, Mat());
-
-		cvtColor(gR_channel, gR_channel, COLOR_GRAY2BGR);
-
-		for (int i = 0; i < regions_red.size(); i++)
-		{
-			Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255));
-			temp_vect = regions_red[i];
-
-			for(int j =0; j < regions_red[i].size() ; j++)
-			{
-				circle( gR_channel, temp_vect[j], 1,  color, -1, 8);
-			}
-
-		}
-	}
-#endif
-/*
-	cout << "--------------------------WRITE TO FILE------------------" << endl;
-	fB_channel << regions_red[0];
-	fG_channel << gG_channel;
-	fR_channel << gR_channel;
-*/
-	cout << "--------------------------WRITE IMAGE--------------------" << endl;
-
-	//Zapis obrazow na dysk
-	memset(file_path, 0, sizeof(file_path));
-	sprintf(file_path, "C:/Users/Pawel/Desktop/baza_znakow/Blue.jpg");
-	imwrite(file_path, gB_channel);
-/*
-	memset(file_path, 0, sizeof(file_path));
-	sprintf(file_path, "C:/Users/Pawel/Desktop/baza_znakow/Yellow.jpg");
-	imwrite(file_path, gY_channel);
-
-	memset(file_path, 0, sizeof(file_path));
-	sprintf(file_path, "C:/Users/Pawel/Desktop/baza_znakow/Blue.jpg");
-	imwrite(file_path, gB_channel);
-
-	memset(file_path, 0, sizeof(file_path));
-	sprintf(file_path, "C:/Users/Pawel/Desktop/baza_znakow/White.jpg");
-	imwrite(file_path, gW_channel);
-
-	memset(file_path, 0, sizeof(file_path));
-	sprintf(file_path, "C:/Users/Pawel/Desktop/baza_znakow/Black.jpg");
-	imwrite(file_path, gK_channel);
-*/
-	cout << "--------------------------END----------------------------" << endl;
-	
-	fB_channel.close();
-	fG_channel.close();
-	fR_channel.close();
-	fOriginalImg.close();
-
-	gOrginalImage.release();
-
-	gBGRImage[0].release();
-	gBGRImage[1].release();
-	gBGRImage[2].release();
-
-	gR_channel.release();
-	gB_channel.release();
-	gY_channel.release();
-	gW_channel.release();
-
-	//dst.release();
-
-    return 0;
-}
-
-#endif
+Mat  gOrginalImage,
+		gGrayImage,
+		gColor_dst,
+		gBGRImage[3],
+		gR_ch_main,
+		gG_ch_main,
+		gB_ch_main,
+		gR_channel,
+		gG_channel,
+		gB_channel,
+		gW_channel,
+		gK_channel, //czarny kanal
+		gY_channel,
+		temp[3];
 
 //Praca na wielu obrazach
 
 #if (!SIMPLE_IMG)
 
-char file_out_name[10], file_name[10], file_path_in[256], file_path_out[256], string_temp[256];
 unsigned int file_number;
-
 
 int main(int argc, char** argv)
 {
-	cout << "--------------------------START--------------------------" << endl;
+	MSERred.DELTA				= 12;
+	MSERred.MIN_AREA			= 1000;
+	MSERred.MAX_AREA			= 15000;
+	MSERred.VARIATION			= 0.3;
 
-	//system("mkdir sample");
+	MSERblue.DELTA				= 7;
+	MSERblue.MIN_AREA			= 1000;
+	MSERblue.MAX_AREA			= 15000;
+	MSERblue.VARIATION			= 0.3;
 
-	cout << "Enter image path: " << endl;
-	cin >> file_path_in;
+	MSERyellow.DELTA			= 12;
+	MSERyellow.MIN_AREA			= 1000;
+	MSERyellow.MAX_AREA			= 15000;
+	MSERyellow.VARIATION		= 0.3;
 
-	cout << "Image name: ";
-	cin >> file_name;
+	MSERwhite.DELTA				= 7;	
+	MSERwhite.MIN_AREA			= 1000;
+	MSERwhite.MAX_AREA			= 15000;
+	MSERwhite.VARIATION			= 0.3;
 
-	#if MSER_RED
-		printf("Default MSER (red): %d, %d, %d, %.2f\r\n", MSER_DELTA_RED, MSER_MIN_AREA_RED, MSER_MAX_AREA_RED, MSER_VARIATION_RED);
-	#endif
+	MSERblack.DELTA				= 7;
+	MSERblack.MIN_AREA			= 1000;
+	MSERblack.MAX_AREA			= 15000;
+	MSERblack.VARIATION			= 0.3;
 
-	#if MSER_BLUE
-		printf("Default MSER (blue): %d, %d, %d, %.2f\r\n", MSER_DELTA_BLUE, MSER_MIN_AREA_BLUE, MSER_MAX_AREA_BLUE, MSER_VARIATION_BLUE);
-	#endif
-
-	cout << "MSER parameters: ";
-	cin >> mser_param;
-
-	//parser
-	{
-		unsigned int i,j,k;
-		char temp_string[10];
-
-		memset(temp_string, 0, sizeof(temp_string));
-		i = j = k = 0;
+	CreateGui();
 		
-		for(i = 0; i <= strlen(mser_param); i++)
-		{
-			if((mser_param[i] != ',')&&(mser_param[i] != 0))
-			{
-				temp_string[k] = mser_param[i];
-				k++;
-			}
-			else if(mser_param[i] == 0)
-			{
-				#if MSER_RED
-					MSER_VARIATION_RED = atof(temp_string);
-				#endif
-
-				#if MSER_BLUE
-					MSER_VARIATION_BLUE = atof(temp_string);
-				#endif
-			}
-			else
-			{
-				switch (j)
-				{
-					case 0:
-						#if MSER_RED
-							MSER_DELTA_RED = atoi(temp_string);
-						#endif
-
-						#if MSER_BLUE
-							MSER_DELTA_BLUE = atoi(temp_string);
-						#endif
-						break;
-					case 1:
-						#if MSER_RED
-							MSER_MIN_AREA_RED = atoi(temp_string);
-						#endif
-
-						#if MSER_BLUE
-							MSER_MIN_AREA_BLUE = atoi(temp_string);
-						#endif
-						break;
-					case 2:
-						#if MSER_RED
-							MSER_MAX_AREA_RED = atoi(temp_string);
-						#endif
-
-						#if MSER_BLUE
-							MSER_MAX_AREA_BLUE = atoi(temp_string);
-						#endif
-						break;
-				}
-				memset(temp_string, 0, sizeof(temp_string));
-				j++;
-				k = 0;
-			}
-		}
-	}
-	
-	#if MSER_RED
-		printf("%d, %d, %d, %.2f\r\n", MSER_DELTA_RED, MSER_MIN_AREA_RED, MSER_MAX_AREA_RED, MSER_VARIATION_RED);
-	#endif
-
-	#if MSER_BLUE
-		printf("%d, %d, %d, %.2f\r\n", MSER_DELTA_BLUE, MSER_MIN_AREA_BLUE, MSER_MAX_AREA_BLUE, MSER_VARIATION_BLUE);
-	#endif
-	
 	memcpy(string_temp, file_path_in, sizeof(file_path_in));
 
 	file_number = 1;
 
-	while(1)
+	while(!SIMPLE_IMG)
 	{
 		memset(file_path_in, 0, sizeof(file_path_in));
 		sprintf(file_path_in, "%s%c%s(%d).jpg", string_temp, 92, file_name, file_number);
@@ -337,12 +128,7 @@ int main(int argc, char** argv)
 		gB_channel.convertTo(gB_channel, CV_8UC1, 255);
 		gW_channel.convertTo(gW_channel, CV_8UC1, 255);
 		gK_channel.convertTo(gK_channel, CV_8UC1, 255);
-		
-		int w(gR_channel.cols);
-		int h(gR_channel.rows);
-
-		Mat dst(5*w, 5*h, CV_8UC3);
-
+	
 		temp[0].zeros(gR_channel.rows, gR_channel.cols, CV_8UC1);
 		temp[0] = gR_channel;
 
@@ -352,96 +138,76 @@ int main(int argc, char** argv)
 		gOrginalImage = gOrginalImage * 255;
 		gOrginalImage.convertTo(gOrginalImage, CV_8UC3); 
 
-		//Wyciecie pierwszego okna
+#if 1
+		//Obraz pomocniczy do wycinania
+		int w(gR_channel.cols);
+		int h(gR_channel.rows);
+		Mat dst(5*w, 5*h, CV_8UC3);
+		
 		gOrginalImage.copyTo(dst(Rect(0, 0, w, h)));
 
-#if MSER_RED
-
-		//MSER 1
+		switch(flMserColor)
 		{
-			MSER ms_red(MSER_DELTA_RED, MSER_MIN_AREA_RED, MSER_MAX_AREA_RED, MSER_VARIATION_RED);
+			case MSER_RED:
+			
+				fcMSER(MSERred, gR_channel, dst);
 
-			vector<vector<Point>> regions_red;
-			vector<Point> temp_vect;
-			ms_red(gR_channel, regions_red, Mat());
+				//Zapis obrazow na dysk
+				cout << "--------------------------WRITE IMAGE--------------------" << endl;
 
-			cvtColor(gR_channel, gR_channel, COLOR_GRAY2BGR);
+				memset(file_path_out, 0, sizeof(file_path_out));
+				sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/RED/%d_mser_red.jpg", file_number);
+				imwrite(file_path_out, dst);
+				break;
 
-			for (int i = 0; i < regions_red.size(); i++)
-			{
-				Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255));
-				temp_vect = regions_red[i];
+			case MSER_BLUE:
+				
+				fcMSER(MSERblue, gB_channel, dst);
 
-				for(int j =0; j < regions_red[i].size() ; j++)
-				{
-					circle( gR_channel, temp_vect[j], 1,  color, -1, 8);
-				}
+				//Zapis obrazow na dysk
+				cout << "--------------------------WRITE IMAGE--------------------" << endl;
 
-			}
+				memset(file_path_out, 0, sizeof(file_path_out));
+				sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/BLUE/%d_mser_blue.jpg", file_number);
+				imwrite(file_path_out, dst);
+				break;
 
-			gR_channel.copyTo(dst(Rect(0, h, w, h)));
+			case MSER_YELLOW:
+				
+				fcMSER(MSERyellow, gY_channel, dst);
+
+				//Zapis obrazow na dysk
+				cout << "--------------------------WRITE IMAGE--------------------" << endl;
+
+				memset(file_path_out, 0, sizeof(file_path_out));
+				sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/YELLOW/%d_mser_yellow.jpg", file_number);
+				imwrite(file_path_out, dst);
+				break;
+
+			case MSER_WHITE:
+								
+				fcMSER(MSERwhite, gW_channel, dst);
+
+				//Zapis obrazow na dysk
+				cout << "--------------------------WRITE IMAGE--------------------" << endl;
+
+				memset(file_path_out, 0, sizeof(file_path_out));
+				sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/WHITE/%d_mser_white.jpg", file_number);
+				imwrite(file_path_out, dst);
+				break;
+
+			case MSER_BLACK:
+				
+				fcMSER(MSERblack, gK_channel, dst);
+
+				//Zapis obrazow na dysk
+				cout << "--------------------------WRITE IMAGE--------------------" << endl;
+
+				memset(file_path_out, 0, sizeof(file_path_out));
+				sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/BLACK/%d_mser_black.jpg", file_number);
+				imwrite(file_path_out, dst);
+				break;
 		}
-	
-		ImCropp(dst, dst, 0, 0, w, 2*h);
-
-		putText(dst, "Original Image", Point2f(50,50), FONT_HERSHEY_SIMPLEX, 1,  Scalar(0,0,255,255), 2);
-		
-		sprintf(tempString, "MSER(delta= %d, min_area= %d, max_area= %d, max_variation= %0.2f)", MSER_DELTA_RED, MSER_MIN_AREA_RED, MSER_MAX_AREA_RED, MSER_VARIATION_RED);
-		putText(dst, tempString, Point2f(50,50+h), FONT_HERSHEY_SIMPLEX, 1,  Scalar(0,0,255,255), 2);
-		memset(tempString, 0, sizeof(tempString));
-
-		cout << "--------------------------WRITE IMAGE--------------------" << endl;
-
-		//Zapis obrazow na dysk
-
-		memset(file_path_out, 0, sizeof(file_path_out));
-		sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/RED/%d_mser_red.jpg", file_number);
-		imwrite(file_path_out, dst);
-
-#endif
-
-#if MSER_BLUE
-
-		{
-			MSER ms_red(MSER_DELTA_BLUE, MSER_MIN_AREA_BLUE, MSER_MAX_AREA_BLUE, MSER_VARIATION_BLUE);
-
-			vector<vector<Point>> regions_blue;
-			vector<Point> temp_vect;
-			ms_red(gB_channel, regions_blue, Mat());
-
-			cvtColor(gB_channel, gB_channel, COLOR_GRAY2BGR);
-
-			for (int i = 0; i < regions_blue.size(); i++)
-			{
-				Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255));
-				temp_vect = regions_blue[i];
-
-				for(int j =0; j < regions_blue[i].size() ; j++)
-				{
-					circle( gB_channel, temp_vect[j], 1,  color, -1, 8);
-				}
-
-			}
-
-			gB_channel.copyTo(dst(Rect(0, h, w, h)));
-		}
-	
-		ImCropp(dst, dst, 0, 0, w, 2*h);
-
-		putText(dst, "Original Image", Point2f(50,50), FONT_HERSHEY_SIMPLEX, 1,  Scalar(0,0,255,255), 2);
-		
-		sprintf(tempString, "MSER(delta= %d, min_area= %d, max_area= %d, max_variation= %0.2f)", MSER_DELTA_BLUE, MSER_MIN_AREA_BLUE, MSER_MAX_AREA_BLUE, MSER_VARIATION_BLUE);
-		putText(dst, tempString, Point2f(50,50+h), FONT_HERSHEY_SIMPLEX, 1,  Scalar(0,0,255,255), 2);
-		memset(tempString, 0, sizeof(tempString));
-
-		cout << "--------------------------WRITE IMAGE--------------------" << endl;
-
-		//Zapis obrazow na dysk
-
-		memset(file_path_out, 0, sizeof(file_path_out));
-		sprintf(file_path_out, "C:/Users/Pawel/Desktop/baza_znakow/MSER/BLUE/%d_mser_blue.jpg", file_number);
-		imwrite(file_path_out, dst);
-
 #endif
 
 #if 0
--- matlab_fcn.h
+++ matlab_fcn.h
@@ -1,11 +1,16 @@
+#ifndef NMATLAB_FCN_H
+#define NMATLAB_FCN_H
+
 #include <opencv2/imgproc/imgproc.hpp>
+#include "main.h"
 
 #define Top_X			1021//320
 #define Top_Y			617//170
 #define Bottom_X		5285//1315
 #define Bottom_Y		1990//470
 
-using namespace cv;
-
 void ImAdjust(Mat& SRC, Mat& DST, float x1, float x2, float y1, float y2);
 void ImCropp(Mat& SRC, Mat& DST, unsigned int TopX, unsigned int TopY, unsigned int BottomX, unsigned int BottomY);
+void fcMSER(_tMSER MSERx, Mat& SRC, Mat& DST);
+
+#endif
